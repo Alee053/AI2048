37c06f394ed70cf7ce26f3b43c21c555efa7b3f9
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-08-31 22:51:34 -0400

    Pre major refactor

---
+++ refactor/Game2048Env.py
@@ -0,0 +1,44 @@
+from game.Fast2048 import Fast2048
+import numpy as np
+from gymnasium import Env
+from gymnasium.spaces import Discrete, Box
+
+from architecture.ConvDQN import board_to_tensor
+
+
+class Game2048Env(Env):
+    def __init__(self):
+        super(Game2048Env, self).__init__()
+        self.game = Fast2048()
+        self.action_space = Discrete(4)
+        self.observation_space = Box(
+            low=0, high=1, shape=(16, 4, 4), dtype=np.float32)
+
+    def reset(self, *, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self.game.reset()
+        state = board_to_tensor(self.game.board)
+        return state, {}
+
+    def step(self, action):
+        reward, done = self.game.move(action)
+        state = board_to_tensor(self.game.board)
+
+        info={}
+
+        if done:
+            info['max_tile']=self.game.max_tile
+            info['score']=self.game.score
+
+        truncated=False
+
+        return state, reward, done,truncated, info
+
+    def render(self, mode='human'):
+        self.game.show_board()
+
+    def close(self):
+        pass
+
+
---
+++ refactor/PPO.py
@@ -0,0 +1,33 @@
+﻿import gymnasium as gym
+from torch import nn
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+import torch
+
+
+class CustomCNN(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 256):
+        super().__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        n_input_channels = observation_space.shape[0]
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 128, kernel_size=2, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=2, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(
+                torch.as_tensor(observation_space.sample()[None]).float()
+            ).shape[1]
+
+        self.linear = nn.Sequential(
+            nn.Linear(n_flatten, features_dim),
+            nn.ReLU()
+        )
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
